794fc00088b54ad8f01956264fb4cb62372c3213
Aybson Mercado <aybsonmg@gmail.com>
2019-08-06 11:26:39 -0600

    create the Queue Node for the scoreboard Queue

--- Nodes/QueueNode.py
+++ Nodes/QueueNode.py
@@ -1,4 +1,21 @@
+from Classes.Player import Player
+
 class QueueNode:
 
-    def __init__(self):
-        pass
+    def __init__(self, new_player=Player() ,next=None):
+        self.new_player = new_player
+        self.next = next
+
+    def get_player(self):
+        return self.new_player
+
+    def get_next(self):
+        return self.next
+
+    def set_player(self, name):
+        self.new_player.set_name(name)
+
+    def set_score(self, score):
+        self.new_player.set_score(score)
+
+
